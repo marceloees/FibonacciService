a1337f9bb6487b6fc0f0fce4c78d298349be3abd
marcelo.evaristo <marcelo.evaristo@5LT37Z2>
2023-11-13 15:15:06 -0300

    Adicionar arquivos de projeto.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("FibonacciService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FibonacciService")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna invisíveis os tipos neste assembly
+// para componentes COM.  Caso precise acessar um tipo neste assembly a partir de 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ff1bf333-a25f-466e-93a3-5d4decb0a53d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números da Revisão e da Versão 
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FibonacciService
+{
+    public class FibonacciService : IFibonacciService
+    {
+        public long ObterProximoFibonacci(int posicao)
+        {
+            if (posicao < 0)
+            {
+                throw new FaultException("A posição deve ser um número não negativo.");
+            }
+
+            long resultado = CalcularFibonacci(posicao + 1);
+            return resultado;
+        }
+
+        private long CalcularFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            long a = 0, b = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                long temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return b;
+        }
+    }
+
+}
